0b18f131620f00e31b811539b4de4ac2e4859f25
Slavcho Slavchev <slavsan.mail@gmail.com>
2024-02-14 20:20:50 +0200

    Initial commit

---
+++ fgo.go
@@ -0,0 +1,124 @@
+package fgo
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Map[T any, M any](f func(T) M) func(T) M {
+	return func(x T) M {
+		return f(x)
+	}
+}
+
+func Filter[T any](f func(T) bool) func(T) bool {
+	return func(x T) bool {
+		return f(x)
+	}
+}
+
+func Take(num int) func() int {
+	return func() int {
+		return num
+	}
+}
+
+func Pipe[T any, M any, R any](xs []T, fs ...interface{}) ([]M, R, error) {
+	ys := make([]M, 0, len(xs))
+	var j R
+	var count int
+	for i, x := range xs {
+		var m M
+		if v, ok := any(x).(M); ok {
+			m = v
+		}
+		for _, f := range fs {
+			// TODO: on first iteration, check the order of functions is valid, i.e. if reduce is defined, it should be last
+			switch f2 := f.(type) {
+			//case func(T) K:
+			//	{
+			//		x = f2(x)
+			//	}
+			case func(T) bool: // filter
+				{
+					if !f2(x) {
+						goto skip
+					}
+				}
+			case func(M) bool: // filter
+				{
+					if !f2(m) {
+						goto skip
+					}
+				}
+			case func(T) T: // map
+				{
+					//fmt.Printf("-----\n")
+					//fmt.Printf("ORIGINAL 1: '%v'\n", x)
+					x = f2(x)
+					//fmt.Printf("MAPPED 1: '%v'\n", x)
+					//fmt.Printf("-----\n")
+					if v, ok := any(x).(M); ok {
+						m = v
+					}
+				}
+			case func(T) M: // map
+				{
+					//fmt.Printf("-----\n")
+					//fmt.Printf("ORIGINAL 2: '%v'\n", x)
+					m = f2(x)
+					//fmt.Printf("MAPPED 2: '%v'\n", m)
+					//fmt.Printf("-----\n")
+				}
+			case reduceStruct[R, T]: // reduce
+				{
+					count++
+					fmt.Printf("REDUCE (original): '%v'\n", x)
+					if count > 1 {
+						f2.initial = j
+					}
+					f2.initial = f2.reduce(f2.initial, x)
+					j = f2.initial
+				}
+			case reduceStruct[R, M]: // reduce
+				{
+					count++
+					fmt.Printf("REDUCE (mapped): '%v'\n", m)
+					//f2.initial = j
+					if count > 1 {
+						f2.initial = j
+					}
+					f2.initial = f2.reduce(f2.initial, m)
+					j = f2.initial
+				}
+			case func() int: // take
+				{
+					num := f2()
+					if i+1 >= num {
+						ys = append(ys, m)
+						goto exit
+					}
+					//f2.reduce(f2.initial, x)
+				}
+			default:
+				return ys, j, fmt.Errorf("invalid function: %s", reflect.TypeOf(f))
+			}
+		}
+		ys = append(ys, m)
+	skip:
+	}
+exit:
+	return ys, j, nil
+}
+
+type reduceStruct[K any, T any] struct {
+	initial K
+	reduce  func(acc K, item T) K
+}
+
+func Reduce[T any, K any](initial K, f func(acc K, item T) K) reduceStruct[K, T] {
+	return reduceStruct[K, T]{
+		initial: initial,
+		reduce:  f,
+	}
+}
---
+++ fgo_test.go
@@ -0,0 +1,387 @@
+package fgo_test
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+
+	f "github.com/slavsan/fgo"
+	"github.com/slavsan/fgo/internal/assert"
+)
+
+func TestFilterStrings(t *testing.T) {
+	testCases := []struct {
+		title     string
+		input     []string
+		expected  []string
+		predicate func(string) bool
+	}{
+		{
+			title:     "can filter out elements with length bigger than 3",
+			input:     []string{"foo", "bar", "spam", "eggs", "baz"},
+			predicate: func(s string) bool { return len(s) > 3 },
+			expected:  []string{"spam", "eggs"},
+		},
+		{
+			title:     "can filter out elements with length bigger smaller than 4",
+			input:     []string{"foo", "bar", "spam", "eggs", "baz"},
+			predicate: func(s string) bool { return len(s) < 4 },
+			expected:  []string{"foo", "bar", "baz"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			result, _, err :=
+				f.Pipe[string, string, *strings.Builder](
+					tc.input,
+					f.Filter[string](tc.predicate),
+				)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestFilterIntegers(t *testing.T) {
+	testCases := []struct {
+		title     string
+		input     []int
+		expected  []int
+		predicate func(int) bool
+	}{
+		{
+			title:     "can filter out elements with bigger than 10",
+			input:     []int{8, 11, 7, 12, 1},
+			predicate: func(i int) bool { return i > 10 },
+			expected:  []int{11, 12},
+		},
+		{
+			title:     "can filter out elements with length smaller than 10",
+			input:     []int{8, 11, 7, 12, 1},
+			predicate: func(i int) bool { return i < 10 },
+			expected:  []int{8, 7, 1},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			result, _, err :=
+				f.Pipe[int, int, *strings.Builder](
+					tc.input,
+					f.Filter[int](tc.predicate),
+				)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestMapIntegerToInteger(t *testing.T) {
+	testCases := []struct {
+		title     string
+		input     []int
+		expected  []int
+		transform func(i int) int
+	}{
+		{
+			title:     "can increment integers",
+			input:     []int{8, 11, 7, 12, 1},
+			transform: func(i int) int { return i + 1 },
+			expected:  []int{9, 12, 8, 13, 2},
+		},
+		{
+			title:     "can decrement integers",
+			input:     []int{8, 11, 7, 12, 1},
+			transform: func(i int) int { return i - 1 },
+			expected:  []int{7, 10, 6, 11, 0},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			result, _, err :=
+				f.Pipe[int, int, *strings.Builder](
+					tc.input,
+					f.Map[int, int](tc.transform),
+				)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestMapStringToString(t *testing.T) {
+	testCases := []struct {
+		title     string
+		input     []string
+		expected  []string
+		transform func(i string) string
+	}{
+		{
+			title:     "can increment integers",
+			input:     []string{"foo", "bar", "baz"},
+			transform: func(s string) string { return s + " x" },
+			expected:  []string{"foo x", "bar x", "baz x"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			result, _, err :=
+				f.Pipe[string, string, any](
+					tc.input,
+					f.Map[string, string](tc.transform),
+				)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestMapStringToInteger(t *testing.T) {
+	testCases := []struct {
+		title     string
+		input     []string
+		expected  []int
+		transform func(i string) int
+	}{
+		{
+			title:     "can increment integers",
+			input:     []string{"foo", "bar", "baz", "spam", "eggs"},
+			transform: func(s string) int { return len(s) },
+			expected:  []int{3, 3, 3, 4, 4},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			result, _, err :=
+				f.Pipe[string, int, any](
+					tc.input,
+					f.Map[string, int](tc.transform),
+				)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestMapIntegerToString(t *testing.T) {
+	testCases := []struct {
+		title     string
+		input     []int
+		expected  []string
+		transform func(i int) string
+	}{
+		{
+			title:     "can increment integers",
+			input:     []int{22, 52, 11, 2, 98},
+			transform: func(i int) string { return strconv.Itoa(i) },
+			expected:  []string{"22", "52", "11", "2", "98"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			result, _, err :=
+				f.Pipe[int, string, any](
+					tc.input,
+					f.Map(tc.transform),
+				)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestFilterAndMap(t *testing.T) {
+	testCases := []struct {
+		title     string
+		input     []int
+		expected  []string
+		predicate func(i int) bool
+		transform func(i int) string
+	}{
+		{
+			title:     "can filter and map",
+			input:     []int{223, 52, 111, 2, 98},
+			predicate: func(i int) bool { return len(strconv.Itoa(i)) > 2 },
+			transform: func(i int) string { return strconv.Itoa(i + 100) },
+			expected:  []string{"323", "211"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			result, _, err :=
+				f.Pipe[int, string, any](
+					tc.input,
+					f.Filter(tc.predicate),
+					f.Map(tc.transform),
+				)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestMapAndFilter(t *testing.T) {
+	testCases := []struct {
+		title     string
+		input     []int
+		expected  []string
+		transform func(i int) string
+		predicate func(i string) bool
+	}{
+		{
+			title:     "can filter and map",
+			input:     []int{223, 52, 111, 2, 98},
+			transform: func(i int) string { return strconv.Itoa(i) },
+			predicate: func(s string) bool { return len(s) <= 2 },
+			expected:  []string{"52", "2", "98"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			result, _, err :=
+				f.Pipe[int, string, any](
+					tc.input,
+					f.Map(tc.transform),
+					f.Filter(tc.predicate),
+				)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestFilterMapReduce(t *testing.T) {
+	testCases := []struct {
+		title     string
+		input     []string
+		expected  []string
+		reducedTo string
+		transform func(i int) string
+		predicate func(i string) bool
+	}{
+		{
+			title:     "with strings builder as reducer",
+			input:     []string{"foo", "bar", "spam", "eggs", "baz"},
+			expected:  []string{"spam 4", "eggs 4"},
+			reducedTo: "spam 4 eggs 4 ",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			result, reduced, err :=
+				f.Pipe[string, string, *strings.Builder](
+					tc.input,
+					f.Filter[string](func(s string) bool { return len(s) > 3 }),
+					f.Map[string, string](func(s string) string { return fmt.Sprintf("%s %d", s, len(s)) }),
+					//f.Take(3),
+					f.Reduce[string, *strings.Builder](&strings.Builder{}, func(sb *strings.Builder, s string) *strings.Builder {
+						sb.WriteString(s)
+						sb.WriteString(" ")
+						return sb
+					}),
+				)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+			assert.Equal(t, tc.reducedTo, reduced.String())
+		})
+	}
+}
+
+func TestMapFilterReduce(t *testing.T) {
+	testCases := []struct {
+		title     string
+		input     []string
+		expected  []string
+		reducedTo string
+		transform func(i int) string
+		predicate func(i string) bool
+	}{
+		{
+			title:     "with strings builder as reducer",
+			input:     []string{"foo", "bar", "spam", "eggs", "baz"},
+			expected:  []string{"foo 3", "bar 3", "baz 3"},
+			reducedTo: "foo 3 bar 3 baz 3 ",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			result, reduced, err :=
+				f.Pipe[string, string, *strings.Builder](
+					tc.input,
+					f.Map[string, string](func(s string) string { return fmt.Sprintf("%s %d", s, len(s)) }),
+					f.Filter[string](func(s string) bool { return strings.HasSuffix(s, "3") }),
+					//f.Take(3),
+					f.Reduce[string, *strings.Builder](&strings.Builder{}, func(sb *strings.Builder, s string) *strings.Builder {
+						sb.WriteString(s)
+						sb.WriteString(" ")
+						return sb
+					}),
+				)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+			assert.Equal(t, tc.reducedTo, reduced.String())
+		})
+	}
+}
+
+func TestFilterStringMapToIntegerThenReduce(t *testing.T) {
+	type sum struct {
+		value int
+	}
+
+	testCases := []struct {
+		title     string
+		input     []string
+		expected  []int
+		reducedTo sum
+		transform func(s string) int
+		predicate func(i string) bool
+	}{
+		{
+			title:     "with strings builder as reducer",
+			input:     []string{"foo", "bar", "spam", "eggs", "baz"},
+			expected:  []int{4, 4},
+			reducedTo: sum{8},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			result, reducedTo, err :=
+				f.Pipe[string, int, sum](
+					tc.input,
+					f.Filter[string](func(s string) bool { return len(s) == 4 }),
+					f.Map[string, int](func(s string) int { return len(s) }),
+					//f.Take(3),
+					f.Reduce[int, sum](sum{}, func(acc sum, i int) sum {
+						acc.value += i
+						return acc
+					}),
+				)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+			assert.Equal(t, tc.reducedTo, reducedTo)
+		})
+	}
+}
